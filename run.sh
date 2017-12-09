a6e29efc889bd1a35535aeb32a109d34115a99b3
dreamingodd <memorywilllast@gmail.com>
2017-12-09 13:46:25 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
+cd /www/var/html/invite
+git pull
+cd /opt/blocks-limited-api/
+git pull
+gradle build
 nohup java -jar build/libs/blocks-limited-api-0.0.1.jar > /opt/log/blocks-limited-api/spring.log  2>&1 &
